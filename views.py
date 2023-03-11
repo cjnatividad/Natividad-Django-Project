7d3a88271b9f58b0b4eba51d1157b4b9d2160e7e
cjnatividad <natividadrjay99@gmail.com>
2023-03-11 14:22:23 +0800

    Added folder to git

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 def dmission(request):
-    mission = """MISSION - The College of Computing and Multimedia Studies shall 
+    mission = """<b>MISSION</b> - The College of Computing and Multimedia Studies shall 
     produce competent and innovative professionals or Technopreneurs in 
     the Information and Communication Technology (ICT) industry adequately
     prepared in the practice of their profession supportive of national 
@@ -11,20 +11,20 @@ def dmission(request):
     return HttpResponse(mission)
 
 def dvision(request):
-    vision = """VISION - The College of Computing and Multimedia Studies
+    vision = """<b>VISION</b> - The College of Computing and Multimedia Studies
     shall be a center of excellence in delivering Computing and Multimedia education."""
 
     return HttpResponse(vision)
 
 def dobjects(request):
-    objects = """OBJECT - After graduation, alumni of MSEUF-CSS program shall:<br>
+    objects = """<b>OBJECT</b> - After graduation, alumni of MSEUF-CSS program shall:<br><br>
      
       1 . Be employed and demostrate professionalism, competence and passion in solving <br>
-      contemporary computing problems by developing or utilizing innovative IT solutions:<br>
+      contemporary computing problems by developing or utilizing innovative IT solutions:<br><br>
        
       2. Emabark in lifelong learning or reseach to attune to the <br>
       continuos innovation in the IT industry in order to adapt to <br>
-      the changing demands of the global market: and<br>
+      the changing demands of the global market: and<br><br>
 
       3. Exhibit leadership and teamwork, and commitment to thier <br>
       respective local or global organizations.<br>
